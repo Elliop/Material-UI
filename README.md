725c7a008ce4e321da82ae48af77670b4d219927
Abdelmouhaymine Salah <asalah@student.1337.ma>
2021-07-05 18:40:13 +0100

    More on makeStyles

--- README.md
+++ README.md
@@ -7,5 +7,7 @@ npm install
 
 npm start
 
+npm install -g json-server
+
 json-server --watch data/db.json --port 8000
 ```
--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -5,11 +5,30 @@ import CardContent from "@material-ui/core/CardContent";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import DeleteOutlined from "@material-ui/icons/DeleteOutlined";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  test: {
+    border: (note) => {
+      if (note.category == "work") {
+        return "1px solid red";
+      } else if (note.category == "reminders") {
+        return "1px solid green";
+      } else if (note.category == "todos") {
+        return "1px solid blue";
+      } else if (note.category == "money") {
+        return "1px solid yellow";
+      }
+    },
+  },
+});
 
 export default function NoteCard({ note, handleDelete }) {
+  const classes = useStyles(note);
+
   return (
     <div>
-      <Card elevation={1}>
+      <Card elevation={1} className={classes.test}>
         <CardHeader
           action={
             <IconButton onClick={() => handleDelete(note.id)}>
